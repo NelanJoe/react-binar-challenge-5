18fcb868f7eec1395d1746434746c8afcc71459e
NelanJoe <nelanjorelat@gmail.com>
2023-10-15 06:40:17 +0700

    ✅ Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+
+      <Footer />
     </BrowserRouter>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+import Carousel from "../components/Carousel";
+import MovieList from "../components/MovieList";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Carousel />
+      <section className="max-w-7xl mx-4 md:mx-auto min-h-screen mt-12">
+        <div className="flex justify-between items-center mb-8">
+          <h2 className="font-semibold text-xl">Popular Movies</h2>
+          <Link className="italic text-red-500">More Popular Movies</Link>
+        </div>
+        <MovieList />
+      </section>
+    </>
+  );
 };
 
 export default Home;
