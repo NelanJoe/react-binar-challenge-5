7a3f4578cc07638a9343bf9cb29bc10a44a4b968
NelanJoe <nelanjorelat@gmail.com>
2023-10-15 06:52:53 +0700

    ✅ Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./components/Header";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
