5cbbeee61ecdc6673f9807f8e4f62cea2a2b7c1a
Ahmads <bariziahnad19@gmail.com>
2023-10-15 17:04:10 +0700

    add : search handle

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Footer from "./components/Footer";
 import DetailMovie from "./pages/DetailMovie";
+import Search from "./pages/Search";
 
 
 const App = () => {
@@ -18,6 +19,7 @@ const App = () => {
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<DetailMovie />} />
         <Route path="/details/:movieId" element={<DetailMovie />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
 
       <Footer />
