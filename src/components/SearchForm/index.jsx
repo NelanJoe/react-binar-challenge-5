b96cb069708ac9796d299f506e4b68ab2d884a59
NelanJoe <nelanjorelat@gmail.com>
2023-10-17 18:50:09 +0700

    🩹 fix: moveitem & searchform

--- src/components/SearchForm/index.jsx
+++ src/components/SearchForm/index.jsx
@@ -2,16 +2,21 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const SearchForm = () => {
+const SearchForm = ({ onClose }) => {
   const navigate = useNavigate();
   const [query, setQuery] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
+    if (!query || query === null) return;
+
     navigate(`/search?page=1&query=${query}`);
 
     setQuery("");
+
+    if (onClose() === null || onClose() === undefined) return;
+    onClose();
   };
 
   return (
@@ -20,6 +25,7 @@ const SearchForm = () => {
         type="text"
         placeholder="Search movie here..."
         className="input input-bordered input-md w-full rounded-full"
+        value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
     </form>
